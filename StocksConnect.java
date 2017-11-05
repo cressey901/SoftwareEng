1287167ec6234672f751d92a365c6b79768458bf
Mathew Smyth <mathew.smyth@outlook.com>
2017-11-05 19:27:00 +0000

    Edited client stock connection file

--- StocksConnect.java
+++ StocksConnect.java
@@ -7,20 +7,75 @@ import java.io.InputStreamReader;
 import java.net.Socket;
 import javax.swing.JOptionPane;
 
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.SocketAddress;
+import java.net.SocketTimeoutException;
+
 
 public class StocksConnect {
+	
+	  public static void main(String[] args)
+	  {
+	    new StocksConnect();
+	  }
 
-	public static void main(String[] args) throws IOException {
+	public StocksConnect ()
+	{
 		// TODO Auto-generated method stub
+		try {
+		Socket clientSocket = new Socket("192.168.0.109", 5000);
+		 String result = writeToReadFrom(clientSocket, "GET /\n\n");
+		 System.out.println(result);
+
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
 		
-		Socket s = new Socket("192.168.0.109", 5000);
-		BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
 		
+		
+		 
+		  
 		System.exit(0);
 		
 
 	}
 	
+	 private String writeToReadFrom(Socket clientSocket, String writeTo) throws Exception
+	 {
+		 try {
+		 PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+		 out.write(writeTo);
+		 out.flush();
+		 
+		 BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+		 StringBuilder sb = new StringBuilder();
+	      String str;
+	      while ((str = input.readLine()) != null)
+	      {
+	        sb.append(str + "\n");
+	      }
+	      
+	      // close the reader, and return the results as a String
+	      input.close();
+	      return sb.toString();
+	    } 
+	    catch (IOException e) 
+	    {
+	      e.printStackTrace();
+	      throw e;
+	    }
+		 
+		 
+	 }
+	 
+}
+	
+	
 	
 
-}
+
